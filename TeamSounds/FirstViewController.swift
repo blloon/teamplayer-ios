84af79f87e0cc3e1823526ed1b5a83f35e758d03
saidmarouf <saidmmm2002@Saids-MacBook-Pro-Retina.local>
2015-07-27 22:01:52 +0200

    Initial Commit

---
+++ TeamSounds/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TeamSounds
+//
+//  Created by Said Marouf on 7/27/15.
+//  Copyright (c) 2015 Said Marouf. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
